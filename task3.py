f1d0c31b5aed4d4fd480d1dc16baa6f981974d51
mykola.mazo.92@gmail.com <mykola.mazo.92@gmail.com>
2024-05-19 18:36:29 +0300

    task3

--- task3.py
+++ task3.py
@@ -1,10 +1,10 @@
 import re
 
-def normalize_phone(phone_number: str) -> str:
+def normalize_phone(phone_numbers: str) -> str:
     del_chars_pattern = r"[^+^\d]"
     add_country_code_pattern = r"^0"
     add_plus_pattern = r"^38"
-    phone_number = re.sub(del_chars_pattern,"", phone_number)
-    phone_number = re.sub(add_country_code_pattern, "+380", phone_number)
-    phone_number = re.sub(add_plus_pattern, "+38", phone_number)
-    return phone_number
+    phone_numbers = re.sub(del_chars_pattern,"", phone_numbers)
+    phone_numbers = re.sub(add_country_code_pattern, "+380", phone_numbers)
+    phone_numbers = re.sub(add_plus_pattern, "+38", phone_numbers)
+    return phone_numbers
